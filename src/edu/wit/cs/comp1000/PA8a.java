e585d47f9577eb6b8e308fe0089987c687d9c85b
Miles Macchiaroli <macchiarolim@wit.edu>
2017-11-15 14:28:34 -0500

    Written, Tested, Commented

--- src/edu/wit/cs/comp1000/PA8a.java
+++ src/edu/wit/cs/comp1000/PA8a.java
@@ -1,9 +1,15 @@
 package edu.wit.cs.comp1000;
-
 import java.io.PrintWriter;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
 
-//TODO: document this class
+/**
+ * PA8a
+ * @author Miles Macchiaroli
+ *
+ */
 public class PA8a {
 	
 	/**
@@ -20,7 +26,15 @@ public class PA8a {
 	 * @param output output destination 
 	 */
 	public static void process(Scanner input, PrintWriter output) {
-		// TODO: write your code here
+		int current; 
+			while(true) { //continuous loop
+				try {//try to take next int
+					current = input.nextInt();
+					if(current > 0) {//if int > 0
+						output.printf("%d%n", current);//print int to file
+					}else {}
+		}
+		catch(NoSuchElementException e){break;}}//catch no more ints
 	}
 
 	/**
@@ -32,7 +46,27 @@ public class PA8a {
 	 * @param args command-line arguments (ignored)
 	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		String fileIn = "", fileOut = "";//create a string for in and out file names
+		Scanner input = new Scanner(System.in);//Call Scanner object
+		System.out.printf("Enter the name of the input file: ");
+		fileIn = input.nextLine();//Set location of input file
+		System.out.printf("Enter the name of the output file: ");
+		fileOut = input.nextLine();//Set desired output location
+	//TEST FILE
+		try (Scanner fin = new Scanner(new File(fileIn)))  {//Attempt input file
+			
+			try (PrintWriter fout = new PrintWriter(new File(fileOut))){ //Attempt Output File
+				process(fin, fout);
+			} catch (FileNotFoundException ex) {//If output cannot be established
+			System.out.printf("Error! File not found!%n");
+			System.exit(0);
+			}
+		} catch (FileNotFoundException ex) {//If fin cannot be found
+			System.out.printf("Error! File not found!%n");
+			System.exit(0);
+		}
+		
 	}
+	
 
 }
--- src/edu/wit/cs/comp1000/PA8b.java
+++ src/edu/wit/cs/comp1000/PA8b.java
@@ -1,6 +1,16 @@
 package edu.wit.cs.comp1000;
 
-//TODO: document this class
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * PA8b
+ * @author Miles Macchiaroli
+ *
+ */
 public class PA8b {
 
 	/**
@@ -13,7 +23,33 @@ public class PA8b {
 	 * @param args command-line arguments (ignored)
 	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		int largest = 0, smallest = 0, current, l=0;
+			String fileIn = "";//create a string for in and out file names
+			Scanner input = new Scanner(System.in);//Call Scanner object
+			System.out.printf("Enter the name of the input file: ");
+			fileIn = input.nextLine();//Set location of input file
+		//TEST FILE
+			try (Scanner fin = new Scanner(new File(fileIn)))  {//Attempt input file
+					while(fin.hasNextInt()) { //while file has int
+						current = fin.nextInt();//try to take next int
+						//
+						if (l == 0){                // Avoid lower number > 0
+							smallest = current;     // By initializing smallest and largest
+							largest = current; l++;}// to be the first number in the file     
+						//
+						if(current > largest) {//if int > largest
+							largest = current;//set current to largest
+						}else if(current < smallest) {//if int < smallest
+							smallest = current;}}// set current to smallest
+					if((fin.hasNext())||((smallest == 0)&&(largest == 0))) {//If more data in document, print error
+			System.out.printf("File: invalid%n");
+			}else {//if no remaining lines, print solution
+					System.out.printf("File: [%d, %d]%n", smallest, largest);}
+					
+				}catch (FileNotFoundException ex) {//If input file cannot be found
+				System.out.printf("File: invalid%n");
+				System.exit(0);
+			}
+			
+		}
 	}
-
-}
